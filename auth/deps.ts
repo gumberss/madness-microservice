9c101b8d2ae45706130eb62f7244579d74bf147f
Gumbers <gustavomarinsuppi@gmail.com>
2020-06-26 22:25:34 -0300

    feat: start auth microservice

---
+++ auth/deps.ts
@@ -0,0 +1,7 @@
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+
+export {
+  Application,
+  Router
+}
+
---
+++ auth/src/main.ts
@@ -0,0 +1,14 @@
+import { Application, Router } from '../deps.ts'
+
+const router = new Router()
+
+router.get('/', context => {
+  context.response.body = "Hi there!"
+})
+
+const app = new Application()
+
+app.use(router.routes())
+app.use(router.allowedMethods())
+
+await app.listen({ port: 9000 })
