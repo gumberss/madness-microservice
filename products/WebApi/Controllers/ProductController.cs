37fe760f17b101f43c04be9472b0b6ffe049091d
Gumbers <gustavomarinsuppi@gmail.com>
2020-07-05 15:12:13 -0300

    feat: creation products webapi

---
+++ products/WebApi/Controllers/ProductController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Products.Models;
+
+namespace Products.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<ProductController> _logger;
+
+        public ProductController(ILogger<ProductController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return new List<Product>{
+            new Product(){
+                Description = "teste",
+
+            }
+            };
+        }
+
+        [HttpPost]
+        public void Post()
+        {
+
+        }
+    }
+}
