1627871025d17985b726b3e043dd846666eb6c18
Gumbers <gustavomarinsuppi@gmail.com>
2020-07-06 20:23:04 -0300

    chore: adding business error class

---
+++ products/Domain/Exceptions/BusinessError.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Domain.Exceptions
+{
+    public class BusinessError
+    {
+        public BusinessError(string message, string field)
+        {
+            Message = message;
+            Field = field;
+        }
+
+        public String Message { get; set; }
+        public String Field { get; set; }
+    }
+}
