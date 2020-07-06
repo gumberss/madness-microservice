8ccad86e88c24c4ca8081a342963e382db9df991
Gumbers <gustavomarinsuppi@gmail.com>
2020-07-06 20:22:04 -0300

    feat: adding product validator bussiness logic

---
+++ products/Domain/Services/ProductValidator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using Domain.Exceptions;
+using Domain.Models;
+
+namespace Domain.Services
+{
+    public class ProductValidator
+    {
+        public List<BusinessError> Validate(Product product)
+        {
+            var errors = new List<BusinessError>(2);
+
+            if (String.IsNullOrEmpty(product.Title))
+            {
+                errors.Add(new BusinessError("The product title must be valid", "Title"));
+            }
+
+            if (product.Price <= 0)
+            {
+                errors.Add(new BusinessError("The product price must be greater than 0", "Price"));
+            }
+
+            return errors;
+        }
+    }
+}
