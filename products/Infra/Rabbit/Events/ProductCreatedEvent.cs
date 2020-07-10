18bc3be3ae884f7b7b4004b113c035fb8dc83eee
Gumbers <gustavomarinsuppi@gmail.com>
2020-07-09 21:23:14 -0300

    chore: adding product created and updated events

---
+++ products/Infra/Rabbit/Events/ProductCreatedEvent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Infra.Rabbit.Events
+{
+    public class ProductCreatedEvent
+    {
+        public Guid Id { get; set; }
+
+        public String Title { get; set; }
+
+        public decimal Price { get; set; }
+
+        public String Description { get; set; }
+    }
+}
---
+++ products/Infra/Rabbit/Events/ProductUpdatedEvent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Rabbit.Events
+{
+    public class ProductUpdatedEvent
+    {
+        public Guid Id { get; set; }
+
+        public String Title { get; set; }
+
+        public decimal Price { get; set; }
+
+        public String Description { get; set; }
+    }
+}
