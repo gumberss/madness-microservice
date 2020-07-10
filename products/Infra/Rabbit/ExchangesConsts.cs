0d463f45115bcc4f1e2864b9dec0582035e5146c
Gumbers <gustavomarinsuppi@gmail.com>
2020-07-09 21:25:01 -0300

    chore: adding RabbitMq exchange consts in a static readonly class

---
+++ products/Infra/Rabbit/ExchangesConsts.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Infra.Rabbit
+{
+    public static class ExchangesConsts
+    {
+        public static String PRODUCT_UPDATED => "product:updated";
+
+        public static String PRODUCT_CREATED => "product:created"; 
+    }
+}
