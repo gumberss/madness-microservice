a04fe4ffe1dde8e517518f64457ccd9f37373506
Gumbers <gustavomarinsuppi@gmail.com>
2020-07-20 23:10:00 -0300

    chore:a dding jason encoder to make phoenix be able to return data in json format

---
+++ client_query/lib/jason_encoder.ex
@@ -0,0 +1,14 @@
+alias Oceanize.User
+# lib/jason_encoder.ex
+defimpl Jason.Encoder, for: User do
+ def encode(%{__struct__: _} = struct, options) do
+   map = struct
+     |> Map.from_struct
+     |> sanitize_map
+   Jason.Encoder.Map.encode(map, options)
+ end
+ defp sanitize_map(map) do
+   # remove some fields (password field is meant to be hidden too)
+   Map.drop(map, [:__meta__, :__struct__, :password])
+ end
+end
