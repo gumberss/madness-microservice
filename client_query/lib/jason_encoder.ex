022a971cb2f67f9625af6273f41f0b3fc6431d70
Gumbers <gustavomarinsuppi@gmail.com>
2020-07-22 00:17:01 -0300

    chore: id as string (always not a good idea)

--- client_query/lib/jason_encoder.ex
+++ client_query/lib/jason_encoder.ex
@@ -4,8 +4,11 @@ defimpl Jason.Encoder, for: Product do
  def encode(%{__struct__: _} = struct, options) do
    map = struct
      |> Map.from_struct
-
+     |> sanitize_map
    Jason.Encoder.Map.encode(map, options)
  end
-
+ defp sanitize_map(map) do
+  # remove some fields (password field is meant to be hidden too)
+  Map.drop(map, [:__meta__, :__struct__])
+end
 end
