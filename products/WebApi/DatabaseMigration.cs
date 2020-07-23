d554921acb807e70cfe0687111600ff53226f56d
Gumbers <gustavomarinsuppi@gmail.com>
2020-07-22 23:25:49 -0300

    chore: try migrate database again when fail

--- products/WebApi/DatabaseMigration.cs
+++ products/WebApi/DatabaseMigration.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Threading;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -13,20 +15,55 @@ namespace WebApi
             using (var scope = webHost.Services.CreateScope())
             {
                 var services = scope.ServiceProvider;
+
+                T context = null;
                 try
                 {
-                    var db = services.GetRequiredService<T>();
-                    db.Database.Migrate();
-                    Console.WriteLine("Database migrated");
+                    context = services.GetRequiredService<T>();
                 }
                 catch (Exception ex)
                 {
                     var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "An error occurred while migrating the database.");
-                    Console.WriteLine("An error occurred while migrating the database.");
+                    logger.LogError(ex, $"An error occured while getting the {typeof(T).Name} context service.");
+                    Console.WriteLine($"An error occured while getting the {typeof(T).Name} context service.");
+                    throw;
                 }
+
+                MigrateDatabase(services, context);
             }
             return webHost;
+        }
+
+        private static void MigrateDatabase<T>(IServiceProvider services, T context, int times = 0) where T : DbContext
+        {
+            try
+            {
+                if (context.Database.GetPendingMigrations().Any())
+                {
+                    context.Database.Migrate();
+                    Console.WriteLine("Database migrated");
+                }
+                else
+                {
+                    Console.WriteLine("There are nothing to migrate");
+                }
+            }
+            catch (Exception ex)
+            {
+                var logger = services.GetRequiredService<ILogger<Program>>();
+                logger.LogError( "An error occurred while migrating the database.");
+                Console.WriteLine("An error occurred while migrating the database.");
+
+                if(times == 15)
+                {
+                    logger.LogError(ex, "Server tried to migrate database 15 times and failed all times.");
+                    Console.WriteLine($"Server tried to migrate database 15 times and failed all times. {ex}");
+                    throw;
+                }
+
+                Thread.Sleep(1500);
+                MigrateDatabase(services, context, ++times);
+            }
         }
     }
 }
--- products/WebApi/Program.cs
+++ products/WebApi/Program.cs
@@ -8,7 +8,7 @@ namespace WebApi
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder(args).Build().MigrateDatabase<ProductsContext>().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
