20d40ac2a1841f9203f7ff57ecd28072876fd222
Gumbers <gustavomarinsuppi@gmail.com>
2020-07-27 21:51:07 -0300

    chore: listening rabbitmq with java working \o/

--- buyer/src/main/java/com/madness/microservice/Main.java
+++ buyer/src/main/java/com/madness/microservice/Main.java
@@ -4,10 +4,12 @@ import javax.inject.Inject;
 import org.eclipse.microprofile.config.ConfigProvider;
 
 import com.madness.microservice.infra.rabbitmq.RabbitMqConnection;
+import com.madness.microservice.infra.rabbitmq.events.ProductCreatedEvent;
 
 import io.quarkus.runtime.Quarkus;
 import io.quarkus.runtime.QuarkusApplication;
 import io.quarkus.runtime.annotations.QuarkusMain;
+import com.madness.microservice.infra.rabbitmq.listeners.ProductCreatedListener;
 
 
 @QuarkusMain
@@ -16,12 +18,16 @@ public class Main implements QuarkusApplication {
   @Inject
   public RabbitMqConnection rabbitConnection;
 
+  @Inject
+  public ProductCreatedListener productCreatedListener;
+
   @Override
   public int run(String... args) throws Exception {
 
     String rabbitMqUri = ConfigProvider.getConfig().getValue("rabbitmq.uri", String.class);
     System.out.println(rabbitMqUri);
     rabbitConnection.withUri(rabbitMqUri).connect();
+    productCreatedListener.listen(ProductCreatedEvent.class);
 
     Quarkus.waitForExit();
 
--- buyer/src/main/java/com/madness/microservice/infra/rabbitmq/listeners/ProductCreatedListener.java
+++ buyer/src/main/java/com/madness/microservice/infra/rabbitmq/listeners/ProductCreatedListener.java
@@ -1,8 +1,11 @@
 package com.madness.microservice.infra.rabbitmq.listeners;
 
+import javax.inject.Singleton;
+
 import com.madness.microservice.infra.rabbitmq.Exchanges;
 import com.madness.microservice.infra.rabbitmq.events.ProductCreatedEvent;
 
+@Singleton
 public class ProductCreatedListener extends Listener<ProductCreatedEvent> {
 
   @Override
